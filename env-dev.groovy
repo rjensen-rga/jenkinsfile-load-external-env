7ff479bf7c4a48a8ea06467df875bd5b3252c58d
Ryan Jensen <ryan.jensen@rgare.com>
2019-09-12 13:21:52 -0500

    making some changes for credentials() method test

--- env-dev.groovy
+++ env-dev.groovy
@@ -1,3 +1,3 @@
 env.repoUrl = 'https://github.com/someuser/theirrepo.git'
 env.env = 'dev'
-env.SOMECREDSID = credentials('7bc0c7fd-794f-406e-be46-58d287d46819')
+env.SOMECREDSID = '7bc0c7fd-794f-406e-be46-58d287d46819'
